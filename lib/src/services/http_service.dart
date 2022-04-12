b1125477b5b2f7af354f322fa0c57d2862faf448
Nikolay <nikolay.batovkin@gmail.com>
2022-04-12 14:14:39 +0300

    add http authorization

--- lib/src/services/http_service.dart
+++ lib/src/services/http_service.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart';
-import 'package:untitled/src/models/sanatorium.dart';
 import '../models/assignment.dart';
 import '../models/relaxer.dart';
 
 class HttpService {
-  final String _url = "https://admin:byport@api.byport.by/";
+  final String _user = "admin";
+  final String _pass = "byport";
+  final String _url = "https://api.byport.by/";
   final String _postfix = "/mobile/";
-  final Map sanMap = Sanatorium.sanatoriumMap;
 
   Future<Relaxer> fetchRelaxer(String sanKey, String email) async {
-    Response res = await get(Uri.parse(_url+sanKey+_postfix+email+"/"));
+    Response res = await get(Uri.parse(_url+sanKey+_postfix+email+"/"),
+        headers: <String, String>{"authorization" : _basicAuth()});
     if (res.statusCode == 200) {
       Relaxer relaxer = Relaxer.fromJson(jsonDecode(res.body));
       relaxer.sanatorium = sanKey;
@@ -21,13 +22,18 @@ class HttpService {
   }
 
   Future<List<Assignment>> fetchAssignments(String sanKey, String email) async {
-    Response res = await get(Uri.parse(_url+sanKey+_postfix+email+"/assignments"));
+    Response res = await get(Uri.parse(_url+sanKey+_postfix+email+"/assignments"),
+        headers: <String, String>{"authorization" : _basicAuth()});
     if (res.statusCode == 200) {
       return (jsonDecode(res.body) as List).map((i) =>
           Assignment.fromJson(i)).toList();
     } else {
       throw "Unable to retrieve assignments.";
     }
+  }
+
+  String _basicAuth() {
+    return 'Basic ' + base64Encode(utf8.encode("$_user:$_pass"));
   }
 
 
